1467b0afc22f2e05cb7d8e61f05224b8319b2327
nonom <nonomartinez@gmail.com>
2022-05-03 17:54:52 +0200

    Added StreamWrapperManager.

--- src/FileLoggerService.php
+++ src/FileLoggerService.php
@@ -2,12 +2,12 @@
 
 namespace Drupal\file_logger;
 
-use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\State\State;
+use Drupal\Core\StreamWrapper\StreamWrapperManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * FileLoggerService
+ * FileLoggerService.
  *
  * Usage:
  * $our_service = \Drupal::service('file_logger.service');
@@ -23,13 +23,24 @@ class FileLoggerService {
   protected $state;
 
   /**
-   * Configuration constructor.
+   * The stream wrapper manager.
+   *
+   * @var \Drupal\Core\StreamWrapper\StreamWrapperManager
+   */
+
+  protected $streamWrapper;
+
+  /**
+   * Spor service configuration constructor.
    *
    * @param \Drupal\Core\State\State $state
    *   The state.
+   * @param \Drupal\Core\StreamWrapper\StreamWrapperManager $streamWrapper
+   *   The stream to get the current URI.
    */
-  public function __construct(State $state) {
+  public function __construct(State $state, StreamWrapperManager $streamWrapper) {
     $this->state = $state;
+    $this->streamWrapper = $streamWrapper;
   }
 
   /**
@@ -39,35 +50,35 @@ class FileLoggerService {
     // Instantiates this form class.
     return new static(
       $container->get('state'),
-      $container->get('messenger'),
+      $container->get('stream_wrapper_manager'),
     );
   }
 
   /**
-   * Returns a log string
-   * @return string $status
+   * Returns a log string.
    */
-  public function log ($status, $ref, $object, $strict = false) {
-      // Not strict log any warning or error.
-      if ($this->state->get('file_logger_enabled') && (!$strict || $status == "WARNING" || $status == "ERROR")) {
-        $realpath = getcwd();
-        // If is an array encode it.
-        if (is_array($object) || is_object($object)) {
-          $object = json_encode($object);
-        }
-        if ($wrapper = \Drupal::service('stream_wrapper_manager')->getViaUri('public://')) {
-          $realpath = $wrapper->realpath();
-        }
-        $file = $realpath.'/log_'.date("dmY").'.csv';
-        // Adds the csv headers.
-        if (!file_exists($file)) {
-          $current = "TIME/HOUR, LEVEL, MODULE, DATA\n";
-          file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
-        }
-        // Print the last log.
-        $file = $realpath.'/log_'.date("dmY").'.csv';
-        $current = date('d-m-Y H:i:s'). ',' . $status.','.$ref.",".$object."\n";
-        file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
+  public function log($status, $ref, $object, $suffix = "", $strict = FALSE) {
+    // Not strict log any warning or error.
+    if ($this->state->get('file_logger_enabled') && (!$strict || $status == "WARNING" || $status == "ERROR")) {
+      $realpath = getcwd();
+      // If is an array encode it.
+      if (is_array($object) || is_object($object)) {
+        $object = json_encode($object);
       }
+      if ($wrapper = $this->streamWrapperManager->getViaUri('public://')) {
+        $realpath = $wrapper->realpath();
+      }
+      $file = $realpath . '/log_' . date("dmY") . '_' . $suffix . '.csv';
+      // Adds the csv headers the very first time.
+      if (!file_exists($file)) {
+        $current = "TIME/HOUR, LEVEL, MODULE, DATA\n";
+        file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
+        return;
+      }
+      // Print the last log.
+      $current = date('d-m-Y H:i:s') . ',' . $status . ',' . $ref . "," . $object . "\n";
+      file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
+    }
   }
+
 }
